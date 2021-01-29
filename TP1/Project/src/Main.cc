6d4c12febd0e49104710a436d2b0799fa2d4dce8
Luiz Almeida <luizorst@hotmail.com>
2021-01-29 01:55:13 -0300

    add tests for queue insert

--- TP1/Project/src/Main.cc
+++ TP1/Project/src/Main.cc
@@ -12,14 +12,16 @@
 
 TEST_SUITE("Order nodes") {
   TEST_CASE("DirectOrder nodes") {
+    // given
     DirectOrder *ord1 = new DirectOrder("ATIVAR", 1);
     DirectOrder *ord2 = new DirectOrder("RELATORIO", 3);
 
+    // when
+    ord1->setNext(ord2);
+    
+    // then
     CHECK(ord1->getOrder() == "ATIVAR");
     CHECK(ord1->getRobot() == 1);
-    CHECK(ord2->getNext() == nullptr);
-
-    ord1->setNext(ord2);
     CHECK(ord1->getNext() == ord2);
     CHECK(ord2->getNext() == nullptr);
 
@@ -28,21 +30,23 @@ TEST_SUITE("Order nodes") {
   }
 
   TEST_CASE("CommandOrders") {
+    // given
     CommandOrder *ord1 = new CommandOrder("COLETAR", 2);
     CommandOrder *ord2 = new CommandOrder("MOVER", 4, 5, 8);
 
+    // when
+    ord1->setNext(ord2);
+
+    // then
     CHECK(ord1->getX() == -1);
     CHECK(ord1->getY() == -1);
     CHECK(ord2->getX() == 5);
     CHECK(ord2->getY() == 8);
     CHECK(ord1->getOrder() == "COLETAR");
     CHECK(ord2->getRobot() == 4);
-
-    CHECK(ord1->getNext() == nullptr);
-    CHECK(ord1->getNext() == ord2->getNext());
-
-    ord1->setNext(ord2);
+    
     CHECK(ord1->getNext() == ord2);
+    CHECK(ord2->getNext() == nullptr);
 
     delete ord1;
     delete ord2;
@@ -51,12 +55,29 @@ TEST_SUITE("Order nodes") {
 
 TEST_SUITE("Queue") {
   TEST_CASE("Instantiation") {
+    // given
     Queue<DirectOrder> q;
 
+    // then
     CHECK(q.getFront() == nullptr);
     CHECK(q.getBack() == nullptr);
     CHECK(q.getSize() == 0);
     CHECK(q.isEmpty() == true);
+  }
+  
+  TEST_CASE("Insert first element") {
+    // given
+    Queue<DirectOrder> q;
+    DirectOrder ord1("ATIVAR", 1);
+    DirectOrder ord2("RELATORIO", 3);
+
+    // when
+    q.insert(ord1);
+
+    // then
+    CHECK(q.isEmpty() == false);
+    CHECK(q.getFront()->getOrder() == ord1.getOrder());
+    CHECK(q.getBack()->getRobot() == ord1.getRobot());
   }
   // TODO: Add testing for insert method
   // TODO: Add testing for dequeue method
