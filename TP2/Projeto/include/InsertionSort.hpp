00d4bbcac3737c9a887bc8ae627c247aa3b898c4
Luiz Almeida <luizorst@hotmail.com>
2021-02-23 22:41:03 -0300

    [FIX] Insertion sort now is DESC

--- TP2/Projeto/include/InsertionSort.hpp
+++ TP2/Projeto/include/InsertionSort.hpp
@@ -10,7 +10,7 @@ void insertionSort(std::vector<std::pair<std::string, int>> &vec)
         std::pair<std::string, int> valueToInsert = vec[i];
         int pos = i;
 
-        while (pos > 0 && vec[pos - 1].second > valueToInsert.second)
+        while (pos > 0 && vec[pos - 1].second < valueToInsert.second)
         {
             vec[pos] = vec[pos - 1];
             pos -= 1;
