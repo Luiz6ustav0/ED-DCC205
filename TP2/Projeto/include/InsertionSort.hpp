1b6d40526a35e33ac3c77879bfc95ac6479ac5d4
Luiz Almeida <luizorst@hotmail.com>
2021-02-23 22:35:37 -0300

    [ADD] Code for Insertion Sort

--- TP2/Projeto/include/InsertionSort.hpp
+++ TP2/Projeto/include/InsertionSort.hpp
@@ -1,15 +1,13 @@
 #include <vector>
 #include <string>
 
-#define siPair std::pair<std::string, int>
-
-void insertionSort(std::vector<siPair> &vec)
+void insertionSort(std::vector<std::pair<std::string, int>> &vec)
 {
     int vecSize = vec.size();
 
     for (int i = 1; i < vecSize; ++i)
     {
-        siPair valueToInsert = vec[i];
+        std::pair<std::string, int> valueToInsert = vec[i];
         int pos = i;
 
         while (pos > 0 && vec[pos - 1].second > valueToInsert.second)
