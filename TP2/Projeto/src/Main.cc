a038dfb8ae7a3404d051030a445ecde2a08de94a
Luiz Almeida <luizorst@hotmail.com>
2021-02-27 21:12:41 -0300

    [FIX] working main for different methods

--- TP2/Projeto/src/Main.cc
+++ TP2/Projeto/src/Main.cc
@@ -1,9 +1,10 @@
 // #include "Test.h"
 #include "../include/HeapSort.hpp"
 #include "../include/InsertionSort.hpp"
+#include "../include/OptimizedQuicksort.hpp"
 #include "../include/QuickSort.hpp"
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
     if (!argv[1] || !argv[2]) {
@@ -19,42 +20,50 @@ int main(int argc, char *argv[]) {
 
     /*** Insertion e Quick Sort ***/
 
-    siPair *myVec = new siPair[linhas];
-    
-    for (int i = 0; i < linhas; ++i) {
-        arqLeitura >> tempPlanetName >> tempHyperjumps;
-        temp.first = tempPlanetName;
-        temp.second = tempHyperjumps;
-        myVec[i] = temp;
-    }
-    
-    insertionSort(myVec, linhas);
-    // QuickSort(myVec, linhas);
+    // siPair *myVec = new siPair[linhas];
+
+    // for (int i = 0; i < linhas; ++i) {
+    //     arqLeitura >> tempPlanetName >> tempHyperjumps;
+    //     temp.first = tempPlanetName;
+    //     temp.second = tempHyperjumps;
+    //     myVec[i] = temp;
+    // }
+
+    // /* ESCOLHA O METODO AQUI */
+    // // insertionSort(myVec, linhas);
+    // // QuickSort(myVec, linhas);
+    // // OptimizedQuickSort(myVec, linhas);
+
+    // if (linhas > 7)
+    //     linhas = 7;
+    // for (int i = 0; i < linhas; ++i)
+    //     std::cout << myVec[i].first << " " << myVec[i].second << std::endl;
 
     /*** *** ***/
 
     /*** Heapsort ***/
 
-    // temp.first = "First Item";
-    // temp.second = -303;
-    // siPair *myVec = new siPair[linhas + 1];
-    // myVec[0] = temp;
-    // int count = 1;
-    // for (int i = 1; i <= linhas; ++i) {
-    //     arqLeitura >> tempPlanetName >> tempHyperjumps;
-    //     temp.first = tempPlanetName;
-    //     temp.second = tempHyperjumps;
-    //     myVec[count] = temp;
-    //     count++;
-    // }
+    temp.first = "First Item";
+    temp.second = -303;
+    siPair *myVec = new siPair[linhas + 1];
+    myVec[0] = temp;
+    int count = 1;
+    for (int i = 1; i <= linhas; ++i) {
+        arqLeitura >> tempPlanetName >> tempHyperjumps;
+        temp.first = tempPlanetName;
+        temp.second = tempHyperjumps;
+        myVec[count] = temp;
+        count++;
+    }
 
-    // HeapSort(myVec, linhas);
+    HeapSort(myVec, linhas);
 
-    /*** *** ***/
-
-    if (linhas > 7) linhas = 7;
+    if (linhas > 7)
+        linhas = 7;
     for (int i = 1; i <= linhas; ++i)
         std::cout << myVec[i].first << " " << myVec[i].second << std::endl;
+
+    /*** *** ***/
 
     delete[] myVec;
 }
