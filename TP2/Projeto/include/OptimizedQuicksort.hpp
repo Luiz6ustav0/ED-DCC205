1958d70f9a18215900c573b5f9227140b4e044ce
Luiz Almeida <luizorst@hotmail.com>
2021-02-27 23:12:53 -0300

    [Improvement] Add median of tree pivot picking for opt_Qs

--- TP2/Projeto/include/OptimizedQuicksort.hpp
+++ TP2/Projeto/include/OptimizedQuicksort.hpp
@@ -4,21 +4,45 @@
 #include "InsertionSort.hpp"
 #include "Queue.hpp"
 #include "siPair.hpp"
+#include "stdlib.h"
+#include "time.h"
 
 typedef struct {
     int left;
     int right;
 } interval;
 
+int Median(int a, int b, int c) {
+    if (a < b) {
+        if (b < c)
+            return b;
+        if (a < c)
+            return c;
+        return a;
+    }
+    if (a < c)
+        return a;
+    if (b < c)
+        return c;
+    return b;
+}
+
+int Pivot(siPair *vec, int low, int high) {
+    int index1 = rand() % (high - low);
+    int index2 = rand() % (high - low);
+    int index3 = rand() % (high - low);
+    return Median(vec[low + index1].second, vec[low + index2].second, vec[low + index3].second);
+}
+
 void OptPartition(int left, int right, int &i, int &j, siPair *vec) {
-    siPair pivot, temp;
+    siPair temp;
     i = left;
     j = right;
-    pivot = vec[(i + j) / 2];
+    int pivot = Pivot(vec, i, j);
     do {
-        while (pivot.second < vec[i].second)
+        while (pivot < vec[i].second)
             i++;
-        while (pivot.second > vec[j].second)
+        while (pivot > vec[j].second)
             j--;
         if (i <= j) {
             temp = vec[i];
@@ -32,7 +56,7 @@ void OptPartition(int left, int right, int &i, int &j, siPair *vec) {
 }
 
 void OptimizedQuickSort(siPair *vec, int n) {
-
+    srand(time(NULL));
     Queue<interval> Q;
     int left = 0, right = n - 1, i, j;
 
@@ -41,7 +65,6 @@ void OptimizedQuickSort(siPair *vec, int n) {
     item.right = right;
 
     Q.insert(item);
-    // Removes the overhead of multiple recursive calls
     do {
         if (right > left) {
             OptPartition(left, right, i, j, vec);
