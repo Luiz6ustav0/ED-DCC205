11d6df50cfafcde6908154b6846feea49a495348
Luiz Almeida <luizorst@hotmail.com>
2021-03-07 02:52:57 -0300

    [FIX] correct graphs

--- TP2/utils/py_bar.py
+++ TP2/utils/py_bar.py
@@ -2,52 +2,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-n_labels = ["0", "100", "500", "1000", "5000", "10000", "50000", "100000", "200000"]
+n_labels = ["0", "100", "500", "1000", "5000",
+            "10000", "50000", "100000", "200000"]
 
-time_hs_ms = [0.038561, 0.230284, 0.494513, 3.00514, 6.38121, 39.2721, 80.9178, 181.019]
-time_hs_s = [i / 1000 for i in time_hs_ms]
+time_hs_ms = [0.01701, 0.098306, 0.211622,
+              1.37833, 2.91382, 15.0288, 31.7789, 66.265]
 
-time_qs_ms = [0.01502, 0.096148, 0.195295, 1.01146, 2.12003, 11.9156, 24.6517, 51.1312]
-time_qs_s = [i / 1000 for i in time_qs_ms]
+time_qs_ms = [0.006589, 0.021788, 0.041724,
+              0.199159, 0.395223, 2.02823, 4.4741, 9.15899]
 
 time_qso_ms = [
-    0.036744,
-    0.178842,
-    0.341509,
-    1.77083,
-    3.66662,
-    20.6795,
-    45.8731,
-    97.4092,
+    0.023298, 0.106826, 0.188282, 0.932573, 2.20175, 11.0447, 23.9959, 52.9057
 ]
-time_qso_s = [i / 1000 for i in time_qso_ms]
 
 time_shell_ms = [
-    0.029607,
-    0.146835,
-    0.360094,
-    2.23231,
-    5.08222,
-    34.346,
-    74.2509,
-    163.771,
+    0.010127, 0.073522, 0.142917, 0.984724, 2.30669, 12.8059, 27.4113, 58.4057
 ]
-time_shell_s = [i / 1000 for i in time_qso_ms]
 
-width_bars = 0.2
+time_insertion_ms = [0.00273, 0.011746, 0.020674,
+                     0.089732, 0.178546, 0.925858, 1.88407, 3.75651]
+
+width_bars = 0.18
 n_size = np.arange(8)
+
+for i in range(8):
+    time_hs_ms[i] *= 2
+    time_qs_ms[i] *= 2
+    time_qso_ms[i] *= 2
+    time_shell_ms[i] *= 2
 
 fig = plt.figure()
 ax = fig.add_axes([0.09, 0.08, 0.8, 0.8])
 
-ax.set_title("Tempo em Função do Tamanho da Entrada - Aleatória")
+ax.set_title("Tempo em Função do Tamanho da Entrada - Decrescente")
 ax.set_ylabel("Tempo em Milisegundos")
 ax.set_xlabel("N linhas consideradas")
 
 rect1 = ax.bar(n_size, time_hs_ms, color="r", width=width_bars)
 rect2 = ax.bar(n_size + width_bars, time_qs_ms, color="b", width=width_bars)
-rect3 = ax.bar(n_size + 2 * width_bars, time_qso_ms, color="g", width=width_bars)
-rect4 = ax.bar(n_size + 3 * width_bars, time_shell_ms, color="grey", width=width_bars)
+rect3 = ax.bar(n_size + 2 * width_bars, time_qso_ms,
+               color="g", width=width_bars)
+rect4 = ax.bar(n_size + 3 * width_bars, time_shell_ms,
+               color="grey", width=width_bars)
+rect4 = ax.bar(n_size + 4 * width_bars, time_insertion_ms,
+               color="yellow", width=width_bars)
 
 
 def autolabel(rects):
@@ -65,14 +63,11 @@ def autolabel(rects):
             )
 
 
-autolabel(rect1)
-autolabel(rect2)
-autolabel(rect3)
-autolabel(rect4)
+ax.grid(True)
 
 ax.set_xticklabels(n_labels)
 ax.legend(
     title="Ordenacao da Entrada",
-    labels=["Heap Sort", "Quick Sort", "QS Otimizado", "Shell Sort"],
+    labels=["Heap Sort", "Quick Sort", "QS Otimizado", "Shell Sort", "Insertion Sort"],
 )
 plt.show()
