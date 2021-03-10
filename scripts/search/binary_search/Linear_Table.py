7076348972d2f6e5093ccaa843416dc1a0078143
Luiz Almeida <luizorst@hotmail.com>
2021-03-09 22:12:30 -0300

    [ADD] binary search

--- scripts/search/binary_search/Linear_Table.py
+++ scripts/search/binary_search/Linear_Table.py
@@ -15,9 +15,14 @@ class Linear_Table:
         return self.__n
 
     def add_items(self, items_arr: Iterable[int]):
-        for i in items_arr:
-            self.__arr.append(i)
-        self.__n = len(self.items)
+        try:
+            for i in items_arr:
+                self.__arr.append(i)
+            self.__n = len(self.items)
+        except TypeError:
+            print(
+                "Items must be passed as inside an iterable. e.g: f([1, 2, 3]) and not f(1, 2, 3)"
+            )
 
     def sequencial_search(self, x: int) -> int:
         if self.size == 0:
@@ -26,5 +31,31 @@ class Linear_Table:
         for i in range(self.size):
             if self.items[i] == x:
                 return i
+
+        return -1
+
+    def binary_search(self, x: int) -> int:
+        """
+        Since it's not guaranteed I'll have sorted arrays and this is just
+        for practice, I'll sorte the array before running the actual thing
+        """
+        if self.items[0] == x:
+            return 0
+
+        self.items.sort()
+
+        left = 0
+        right = self.size
+        i = right // 2
+
+        while left <= right:
+            if self.items[i] == x:
+                return i
+            if self.items[i] > x:
+                right = i - 1
+            else:
+                left = i + 1
+
+            i = (left + right) // 2
 
         return -1
